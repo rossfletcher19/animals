eb49b5ea68b5c18365640ec09919c0d18368269c
Paul Kotok and Ross Fletcher <ross@>
2017-11-30 11:10:17 -0800

    add further js to show and hide divs

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,27 @@
 $(document).ready(function() {
-  var age = parseInt(prompt("How old are you?"));
 
-  if (age >= 18) {
-    $('#votinginfo').show();
-  } else {
-    $('#cantvote').show();
-  }
+
+    $("button#frogbtn").click(function() {
+      $('#frog').show();
+      $('#bird').hide();
+      $('#albino-gorilla').hide();
+    });
+
+    $("button#gorillabtn").click(function() {
+      $('#albino-gorilla').show();
+      $('#frog').hide();
+      $('#bird').hide();
+    });
+
+    $("button#mothbtn").click(function() {
+      $('#bird').show();
+      $('#frog').hide();
+      $('#albino-gorilla').hide();
+
+    });
+
+
+
+
+
 });
