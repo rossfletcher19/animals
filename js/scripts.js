1560ca162e4f27a6cc5d9eeda171cb2fa72751f7
Paul Kotok and Ross Fletcher <ross@>
2017-11-30 16:51:13 -0800

    fixed js to show divs

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,19 @@
 $(document).ready(function() {
-
-  $(".btn btn-success").click(function() {
+  $("#formOne").submit(function(event) {
+    event.preventDefault();
     var animal = $("input:radio[name=animal]:checked").val();
-
       if (animal === "frog") {
         $("#frog").show();
         $("#albino-gorilla").hide();
         $("#bird").hide();
-      }
-
-      else if (animal === "albino-gorilla") {
+      } else if (animal === "albino-gorilla") {
         $("#albino-gorilla").show();
         $("#frog").hide();
         $("#bird").hide();
-      }
-
-      else (animal === "bird") {
+      } else if (animal === "bird") {
         $("#bird").show();
         $("#albino-gorilla").hide();
         $("#frog").hide();
       }
-
   });
 });
